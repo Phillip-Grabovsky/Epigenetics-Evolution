5f32f1838031889c15689520f1bd2b207283491a
Phillip Grabovsky <phillip.grabovsky@gmail.com>
2019-06-09 19:22:33 -0400

    Run Instance: DTR = 2

--- CONSTANTS.java
+++ CONSTANTS.java
@@ -4,10 +4,8 @@ import java.lang.Math;
 public class CONSTANTS{
   //All Simulation Parameters are here.
   public static int secondsInDay = 50; //amount of seconds until food is given
-  public static int totalTime = 10000; //time in days until simulation ends
   public static int startingEnergy = 100;
   public static int startingBacteria = 2; //number of starting bacteria
-  public static int totalSeconds = totalTime * secondsInDay;
   public static int enzymeCost = 1; //energy cost to produce 1 enzyme
   public static int foodValue = 60; //energy given by digesting food
   public static double sMutationChance = 0.1; //probablility of a signalling mutation occuring
@@ -15,9 +13,13 @@ public class CONSTANTS{
   public static double signalMutationSize  = 1; //size of a mutation in the signalling pathway
   public static double baselineMutationSize = 1; //size of a mutation in base methyl production.
   public static int numberOfBases = 100; //number of methylatable bases in genome for enzyme.
-  public static int replicationEnergy = 600; //energy level needed to replicate
+  public static int replicationEnergy = 120; //energy level needed to replicate
   public static Random theRandom = new Random(600);
-  public static int giveFoodAtTime = 0; //ranges from 0 to the value of
+  public static int giveFoodAtTime = 49; //ranges from 0 to the value of
+
+  //time in days until simulation ends
+  public static int totalTime = (replicationEnergy - startingEnergy)/(foodValue - secondsInDay*enzymeCost)*1000;
+  public static int totalSeconds = totalTime * secondsInDay;
 
   public static double random(){
     //double randomNum = theRandom.nextDouble();
